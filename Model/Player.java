f06c5706a02dcb48729eb6c4cb6c58512243c8f1
Kalle Wiklund <kallewiklund2002@gmail.com>
2023-11-01 15:48:13 +0100

    player fixed

--- Model/Player.java
+++ Model/Player.java
@@ -1,5 +1,27 @@
 package Model;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Player implements Collidable, Moveable {
-    private Image image;
+    // String imagePath = "path";
+    BufferedImage myPicture;
+
+    public Player(String imagePath) {
+        try {
+            this.myPicture = ImageIO.read(new File(imagePath));
+        } catch (IOException exception) {
+            System.out.println(exception);
+        }
+        
+    }
+    
+
+
+    public void move() {
+
+    }
 }
