ffd748895c848c1cc5540dfe5d1da4de205183a4
Heldereim <adam.hellgard@hotmail.se>
2023-11-02 11:01:50 +0100

    Added functionality for checking whether the bullet is outside the bounds of the game board

--- Model/Bullet.java
+++ Model/Bullet.java
@@ -2,18 +2,25 @@ package Model;
 
 public class Bullet extends Sprite implements Collidable, Moveable{
 
-    
-    private double size;
-    private double dir;
+    double dx, dy;
 
-    public void Bullet( double size, double dir){
+    public Bullet(String imagePath) {
+        super(0, 0, imagePath);
+        
+    }
 
-        this.size = size;
-        this.dir = dir;
+    public boolean isOutOfBounds(int boundX, int boundY) {
+        if (this.x >= 0 && this.x <= boundX) {
+            if (this.y >= 0 && this.y <= boundY) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public void move(double deltaTime) {
-
+        this.x += dx;
+        this.y += dy;
     }
 
 }
