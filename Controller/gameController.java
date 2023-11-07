a20d038b769d9209b2bcad13dc2b8554aa64dd57
sebbekallasjag <sebastian.mail.andersson@gmail.com>
2023-11-07 11:11:20 +0100

    Add basic outline for gameController

---
+++ Controller/gameController.java
@@ -0,0 +1,24 @@
+class gameController {
+    // remove comments and change to correct gameModel and gameView-name 
+    // private gameModel model; 
+    // private gameView view; 
+
+    public gameController(gameModel model, gameView view) {
+        this.model = model; 
+        this.view = view; 
+    }
+
+    public void handleInput() {
+
+    }
+
+    public void gameLoop() {
+        while (true) {
+            handleInput();
+            update();
+            repaint();
+
+        }
+
+    }
+}
