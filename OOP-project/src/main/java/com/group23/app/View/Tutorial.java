280b4b569b905e7e10e7aad3838929650a14901b
Carl Toreborg <114061060+carltoreborg@users.noreply.github.com>
2023-11-16 15:16:15 +0100

    Added title text and fixed margins

--- OOP-project/src/main/java/com/group23/app/View/Tutorial.java
+++ OOP-project/src/main/java/com/group23/app/View/Tutorial.java
@@ -1,6 +1,7 @@
 package com.group23.app.View;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.TextArea;
@@ -15,6 +16,7 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Tutorial extends JFrame implements KeyListener{
     
@@ -23,15 +25,30 @@ public class Tutorial extends JFrame implements KeyListener{
 
     ContentPane tutorialPane = ContentPane.getContentPane();
 
-    JLabel text = new JLabel("The goal of the game is to avoid being hit by the laserbeams. You move by using.... The laserbeam will come in from directions around the screen in a rate that is increasing with the longer you play.");
+    JLabel text = new JLabel("<html>The goal of the game is to avoid being hit by the laserbeams.<br> You move by using:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; The laserbeam will come in from directions around the screen in a rate that is increasing with the longer you play.</html>");
 
+    JLabel title = new JLabel("Tutorial");
 
     public Tutorial(){
         super("Game");
 
         text.setForeground(Color.WHITE);
-        tutorialPane.add(text);
+
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font(text.getFont().getName(), Font.PLAIN, 30));
+        text.setHorizontalAlignment(SwingConstants.CENTER);
+        text.setVerticalAlignment(SwingConstants.TOP);
+        text.setBounds(70,80, SCREEN_WIDTH-100, SCREEN_HEIGHT);
         
+        title.setForeground(Color.WHITE);
+        title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 50));
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        title.setVerticalAlignment(SwingConstants.TOP);
+        title.setBounds(0,0, SCREEN_WIDTH, SCREEN_HEIGHT/4);
+
+        tutorialPane.add(title);
+        tutorialPane.add(text);
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
         addKeyListener(this);
