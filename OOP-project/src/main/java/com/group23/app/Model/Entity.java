518705b7de9b129b83970802e188a0f018b5ed0e
Heldereim <adam.hellgard@hotmail.se>
2023-11-21 13:52:35 +0100

    Added an Entity class that Player and Laser should inherit from

---
+++ OOP-project/src/main/java/com/group23/app/Model/Entity.java
@@ -0,0 +1,19 @@
+package com.group23.app.Model;
+
+public class Entity {
+    private int x,y;
+    private int width,height;
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+}
