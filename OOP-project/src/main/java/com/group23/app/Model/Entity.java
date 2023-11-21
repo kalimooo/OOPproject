f819352671c106dfc7d3acef56aeda80bbb64e56
Heldereim <adam.hellgard@hotmail.se>
2023-11-21 14:00:21 +0100

    Added a constructor for the Entity class and made the class abstract

--- OOP-project/src/main/java/com/group23/app/Model/Entity.java
+++ OOP-project/src/main/java/com/group23/app/Model/Entity.java
@@ -1,8 +1,15 @@
 package com.group23.app.Model;
 
-public class Entity {
+public abstract class Entity {
     private int x,y;
     private int width,height;
+
+    Entity(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
 
     public int getX() {
         return x;
