5226596d2d584aeb6c934bbffba9ebab71eca566
Kalle Wiklund <kallewiklund2002@gmail.com>
2023-11-21 13:27:07 +0100

    Updated model and started working on controller

---
+++ OOP-project/src/main/java/com/group23/app/Controller/Controller.java
@@ -0,0 +1,11 @@
+package com.group23.app.Controller;
+
+import com.group23.app.Model.Moveable;
+
+public class Controller {
+    Moveable controlledUnit;
+    
+    public Controller(Moveable controlledUnit) {
+        this.controlledUnit = controlledUnit;
+    }
+}
