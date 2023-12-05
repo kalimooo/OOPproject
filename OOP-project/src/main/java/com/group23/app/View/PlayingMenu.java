91e7874c423cbb81497dfdf03b38e6064074b928
sebbekallasjag <sebastian.mail.andersson@gmail.com>
2023-12-05 14:16:14 +0100

    Menu work, fixed button but needs alignment

--- OOP-project/src/main/java/com/group23/app/View/PlayingMenu.java
+++ OOP-project/src/main/java/com/group23/app/View/PlayingMenu.java
@@ -1,39 +1,43 @@
 package com.group23.app.View;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.BorderLayout;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
-import java.util.Scanner;
 
 public class PlayingMenu extends JPanel {
-        Color menuBackgroundColor = Color.RED;
-        Color buttonColor = Color.decode("#767675");
-        private JLabel helloLabel;
-        private JButton quitButton; 
+    Color menuBackgroundColor = Color.decode("#4f504e");
+    Color buttonColor = Color.decode("#767675");
+    private JLabel helloLabel;
+    private JButton quitButton; 
 
-        static PlayingMenu playingMenu;
+    static PlayingMenu playingMenu;
+
     // Konstruktor
     public PlayingMenu() {
-        // Skapa en panel för hela menyn och sätt bakgrundsfärgen
-        JPanel menuPanel = new JPanel(null);
-        menuPanel.setBackground(menuBackgroundColor); // Funkar inte just nu
+        // Använd BorderLayout för att placera komponenter
+        setLayout(new BorderLayout());
         
+        // Sätt bakgrundsfärgen för hela menyn
+        setBackground(menuBackgroundColor);
+
         helloLabel = new JLabel("PRESS SPACE TO START!");
         helloLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 40));
         helloLabel.setForeground(Color.black); // Ändra textfärgen till vit
         helloLabel.setBorder(new EmptyBorder(0, 10, 0, 0)); // Lägg till en kant till vänster om texten
-        add(helloLabel);
-        setBounds(0,0,800,60);
-        
-        
+        add(helloLabel, BorderLayout.WEST); // Placera helloLabel längst till vänster
+
+        quitButton = new JButton("QUIT");
+        quitButton.setFocusable(false); // Förhindra att knappen tar emot fokus
+        quitButton.setMargin(new Insets(0, 20, 0, 20)); // Lägg till en kant till höger om knappen
+        add(quitButton, BorderLayout.EAST); // Placera quitButton längst till höger
+
+        setBounds(0, 0, 800, 60);
 
         PlayingMenu.playingMenu = this;
     }
