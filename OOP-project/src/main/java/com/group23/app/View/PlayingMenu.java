94800bdce6d41d7043bc4643aa1489b679bed704
sebbekallasjag <sebastian.mail.andersson@gmail.com>
2023-12-05 14:35:31 +0100

    added actionlisteners for quitbutton, method to update label text in menu

--- OOP-project/src/main/java/com/group23/app/View/PlayingMenu.java
+++ OOP-project/src/main/java/com/group23/app/View/PlayingMenu.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -26,9 +28,9 @@ public class PlayingMenu extends JPanel {
         // Sätt bakgrundsfärgen för hela menyn
         setBackground(menuBackgroundColor);
 
-        helloLabel = new JLabel("PRESS SPACE TO START!");
+        helloLabel = new JLabel("");
         helloLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 40));
-        helloLabel.setForeground(Color.black); // Ändra textfärgen till vit
+        helloLabel.setForeground(Color.WHITE); // Ändra textfärgen till vit
         helloLabel.setBorder(new EmptyBorder(0, 10, 0, 0)); // Lägg till en kant till vänster om texten
         
         // Align the label to the left
@@ -48,8 +50,22 @@ public class PlayingMenu extends JPanel {
         quitButton.setFocusable(false); // Förhindra att knappen tar emot fokus
         add(quitButtonPanel, BorderLayout.EAST); // Placera quitButton längst till höger
 
+        // ActionListener för quitbutton
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("USER PRESSED QUIT!");
+            }
+        });
+
+        
+
         setBounds(0, 0, 800, 60);
 
         PlayingMenu.playingMenu = this;
+    }
+
+    public void updateTime(String elapsedTime) {
+        this.helloLabel.setText(elapsedTime);
     }
 }
