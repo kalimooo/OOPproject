09c420e9c31bad658d479dcabb1c28129e40858b
sebbekallasjag <sebastian.mail.andersson@gmail.com>
2023-12-11 10:03:57 +0100

    Made PlayingMenu font more modular, assigning variable. Minor font changes.

--- OOP-project/src/main/java/com/group23/app/View/PlayingMenu.java
+++ OOP-project/src/main/java/com/group23/app/View/PlayingMenu.java
@@ -12,58 +12,67 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
 
 import com.group23.app.Model.Model;
 
 public class PlayingMenu extends JPanel {
+    Font menuFont = new Font(Font.SANS_SERIF, Font.BOLD, 30);
     Color menuBackgroundColor = Color.decode("#4f504e");
     Color buttonColor = Color.decode("#767675");
     private JLabel helloLabel;
     private JButton quitButton;
+    private JButton tutorialButton;
 
     static PlayingMenu playingMenu;
 
-    // Konstruktor
     private PlayingMenu() {
-        // Använd BorderLayout för att placera komponenter
         setLayout(new BorderLayout());
-
-        // Sätt bakgrundsfärgen för hela menyn
         setBackground(menuBackgroundColor);
 
         helloLabel = new JLabel("00:00:01");
-        helloLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 40));
-        helloLabel.setForeground(Color.WHITE); // Ändra textfärgen till vit
-        helloLabel.setBorder(new EmptyBorder(0, 10, 0, 0)); // Lägg till en kant till vänster om texten
-
-        // Align the label to the left
+        helloLabel.setFont(menuFont);
+        helloLabel.setForeground(Color.WHITE);
+        helloLabel.setBorder(new EmptyBorder(0, 10, 0, 0));
         helloLabel.setAlignmentX(JLabel.LEFT_ALIGNMENT);
 
-        add(helloLabel);
-        setBounds(0, 0, GameWindow.SCREEN_WIDTH, 60);
-
-        add(helloLabel, BorderLayout.WEST); // Placera helloLabel längst till vänster
+        add(helloLabel, BorderLayout.WEST);
 
         quitButton = new JButton("[Q] QUIT");
-        JPanel quitButtonPanel = new JPanel(new BorderLayout());
-        quitButtonPanel.add(quitButton, BorderLayout.EAST);
-        quitButtonPanel.setBorder(new EmptyBorder(0, 0, 0, 40));
-        quitButtonPanel.setBackground(menuBackgroundColor);
-        quitButton.setFocusable(false); // Förhindra att knappen tar emot fokus
-        quitButton.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 40));
+        quitButton.setFocusable(false);
+        quitButton.setFont(menuFont);
         quitButton.setBackground(buttonColor);
         quitButton.setForeground(Color.WHITE);
-        add(quitButtonPanel, BorderLayout.EAST); // Placera quitButton längst till höger
 
-        // ActionListener för quitbutton
         quitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("USER PRESSED QUIT!");
                 showQuitDialog();
-
             }
         });
+
+        tutorialButton = new JButton("[T] TUTORIAL");
+        tutorialButton.setFocusable(false);
+        tutorialButton.setFont(menuFont);
+        tutorialButton.setBackground(buttonColor);
+        tutorialButton.setForeground(Color.WHITE);
+
+        tutorialButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("USER PRESSED TUTORIAL!");
+                // Add your code to handle the tutorial button press (open tutorial, etc.)
+            }
+        });
+
+        // Use FlowLayout for the button panel
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        buttonPanel.setBackground(menuBackgroundColor);
+        buttonPanel.add(tutorialButton);
+        buttonPanel.add(quitButton);
+
+        add(buttonPanel, BorderLayout.EAST);
 
         setBounds(0, 0, GameWindow.SCREEN_WIDTH, 60);
 
